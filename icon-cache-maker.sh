4de997dcbd15ae080e10f8154d79733f80f238e9
zardoz-ilbarbaro <davide.romboli@gmail.com>
2025-12-24 13:13:40 +0100

    update

---
+++ icon-cache-maker.sh
@@ -0,0 +1,15 @@
+### Auto update the iconcache
+### Do a manual update with following code in a terminal if you get errors
+### Do check if you have used spaces in the naming of files or folders
+### Sometimes you make a copy of an icon - this contains a space
+### gtk-update-icon-cache -f iconthemefolder
+
+### in some of the distributions you will a message popup if an icon theme has no cache
+### if you have changed the icons - deleted folders etcetera then run this script
+gtk-update-icon-cache -f usr/share/icons/al-beautyline
+gtk-update-icon-cache -f usr/share/icons/al-candy-icons
+gtk-update-icon-cache -f usr/share/icons/neo-candy-icons
+
+rm -v usr/share/icons/al-beautyline/icon-theme.cache
+rm -v usr/share/icons/al-candy-icons/icon-theme.cache
+rm -v usr/share/icons/neo-candy-icons/icon-theme.cache
---
+++ up.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+# reset - commit your changes or stash them before you merge
+# git reset --hard - personal alias - grh
+
+if [[ -f "./repo.sh" ]]; then
+    echo "Found repo.sh, running it..."
+    bash ./repo.sh
+fi
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+git commit -m "update"
+
+# Push the local files to github
+
+if grep -q main .git/config; then
+	echo "Using main"
+		git push -u origin main
+fi
+
+if grep -q master .git/config; then
+	echo "Using master"
+		git push -u origin master
+fi
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
